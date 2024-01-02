7628ecbd632155b42bf2096fac9f39d82ebeeec5
Kesinee Powers <56649145+KesineeP@users.noreply.github.com>
2024-01-02 16:50:33 -0500

    Challenge #2: Exercise 2

--- Demo/src/Challenge2.java
+++ Demo/src/Challenge2.java
@@ -10,9 +10,21 @@ public class Challenge2 {
         
         System.out.println("\nHow old are you?");
         int userAge = scanner.nextInt();
-        scanner.close();
         
         System.err.println("\nYou've lived " + userAge + " years. In another " + userAge + " you'll be " + userAge*2 + " years old.");
         System.out.format("You have %d characters in your name.", userName.length());
+
+        //Exercise 2
+        System.out.println("\nRestaurant Bill Calculation");
+        System.out.println("How much is the bill total?");
+        double billAmount = scanner.nextDouble();
+
+        System.out.println("How many people?");
+        int peopleNumber = scanner.nextInt();
+        scanner.close();
+        
+        double amount = billAmount / peopleNumber;
+        System.out.format("Each person should pay %.2f dollars.", amount);
+        System.out.println();
     }
 }
