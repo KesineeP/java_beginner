4c896763ca8aaca4eeb1014c637f5c038ff5e93d
Kesinee Powers <56649145+KesineeP@users.noreply.github.com>
2024-01-03 11:55:54 -0500

    Challenge #4: Exercise 5-6

--- Demo/src/Challenge4.java
+++ Demo/src/Challenge4.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Challenge4 {
     public static void loops() {
+        Scanner scanner = new Scanner(System.in);
 
         //Exercise 1
         //for(int i = 89; i <= 117; i++) {
@@ -18,26 +20,58 @@ public class Challenge4 {
         //}
 
         //Exercise 3
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Please enter your password: ");
-//        String inputPassword = scanner.next();
-        
-//        do {
-//          System.out.println("Incorrect password, please try again");
-//          System.out.println("Enter your password: ");
-//          inputPassword = scanner.next();
-//        } while(!inputPassword.equals("shark50"));
-//      
-//          System.out.println("ACCESS APPROVED");
-//          scanner.close();
+        // System.out.println("Please enter your password: ");
+        // String inputPassword = scanner.next();
+ 
+        // do {
+        //   System.out.println("Incorrect password, please try again");
+        //   System.out.println("Enter your password: ");
+        //   inputPassword = scanner.next();
+        // } while(!inputPassword.equals("shark50"));
+      
+        //   System.out.println("ACCESS APPROVED");
+       
 
         //Exercise 4
-        int i = 0;
-        String output = "";
-        while(i < 11) {
-            output = output + i + ", ";
+        //First approach
+         int i = 0;
+         String output = "";
+         while(i < 11) {
+            if(i == 10) {
+                output = output + i;
+            }else {
+                output = output + i + ", ";
+            }
             i++;
-        }
-        System.out.println(output);
+         }
+         System.out.println(output + ".");
+
+        //Second approach
+        //for(int i = 0; i <= 10; i++) {
+        //   System.out.print(i + ", ");
+        //}
+        //System.out.println("\b\b.");
+
+        //Exercise 5 + 6
+        int randomNumber = ThreadLocalRandom.current().nextInt(1,101);
+        System.out.println("Guess a number 1-100:");
+        int guess = scanner.nextInt();
+        int countGuess = 0;
+
+        do {
+            if(guess > randomNumber) {
+                System.out.println("LOWER");
+                System.out.println("Guess number again:");
+                guess = scanner.nextInt();
+                countGuess++;
+            }else {
+                System.out.println("HIGHER");
+                System.out.println("Guess number again:");
+                guess = scanner.nextInt();
+                countGuess++;
+            }
+        }while(randomNumber != guess);      
+        System.out.println("Correct! It took you " + countGuess + " times to guess the correct number!");
+        scanner.close();
     }
 }
