68d341f326089cede31c40a1f752689b07d310b3
Kesinee Powers <56649145+KesineeP@users.noreply.github.com>
2024-01-05 14:11:39 -0500

    Challenge #6: Exercise 3

--- Demo/src/Challenge6.java
+++ Demo/src/Challenge6.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Challenge6 {
     static Scanner scanner = new Scanner(System.in);
     
     public static void examResults() {
-        exercise2();
+        exercise3();
     }
    
     public static void exercise1() {
@@ -47,6 +48,7 @@ public class Challenge6 {
     }
 
     public static void exercise2() {
+
         String[] questions = new String[] {
             "We are in 20th century",
             "The United States of America is located in North America",
@@ -89,5 +91,64 @@ public class Challenge6 {
                 System.out.println("Google can help you! Your score: " + score);
                 break;
         }
+    }
+
+    public static void exercise3() {
+        // int randomNumber = ThreadLocalRandom.current().nextInt(1,101);
+        // System.out.println("Guess a number 1-100:");
+        // int guess = scanner.nextInt();
+        // int countGuess = 0;
+
+        // do {
+        //     if(guess > randomNumber) {
+        //         System.out.println("LOWER");
+        //         System.out.println("Guess number again:");
+        //         guess = scanner.nextInt();
+        //         countGuess++;
+        //     }else {
+        //         System.out.println("HIGHER");
+        //         System.out.println("Guess number again:");
+        //         guess = scanner.nextInt();
+        //         countGuess++;
+        //     }
+        // }while(randomNumber != guess);      
+        // System.out.println("Correct! It took you " + countGuess + " times to guess the correct number!");
+        System.out.println("-----Guess my number game-----");
+        System.out.println("-----Rules-----");
+        System.out.println("Think of a number from 1-100 and I will try to guess it");
+        System.out.println("Enter \"higher\" if my guess is too low");         
+        System.out.println("Enter \"lower\" if my guess is too high");  
+        
+        String input;
+        int min = 1;
+        int max = 100;
+
+        while(true) {
+            int randomNumber = ThreadLocalRandom.current().nextInt(min,max+1);
+            System.out.println("I'm guessing: " + randomNumber );
+            System.out.println("Your response: ");
+            input = scanner.next();
+            
+            if(input.equals("higher")) {
+                min = randomNumber + 1;
+            }else if(input.equals("lower")) {
+                max = randomNumber - 1;
+            }else if(input.equals("correct")) {
+                System.out.println("Congrat! " + randomNumber + " is correct!");
+                break;
+            }
+            
+            if(min == max+1) {
+                System.out.println("That is not possible. Let's try again");
+                min = 1;
+                max = 101;
+            }
+            
+        }
+
+        
+        
+      
+
     }
 }
